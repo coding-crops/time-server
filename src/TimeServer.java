cf95f2b4bd631d8482d2d2280ff8b6aa684aee05
xin.liu <731249434@qq.com>
2018-06-10 23:13:03 +0800

    initalize

---
+++ src/TimeServer.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2018/6/10.
+ */
+public class TimeServer {
+
+        /**
+         * @param args
+         * @throws IOException
+         */
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                // 采用默认值
+            }
+        }
+        MultiplexerTimeServer timeServer = new MultiplexerTimeServer(port);
+        new Thread(timeServer, "NIO-MultiplexerTimeServer-001").start();
+    }
+}
+
